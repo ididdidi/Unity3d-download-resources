b66d53734470f0925b97992138f020dcb7d1bf39
mofrison <mofrison@ya.ru>
2021-03-30 20:19:14 +0300

    Code review

--- Assets/Source/Code/ResourceCache.cs
+++ Assets/Source/Code/ResourceCache.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using UnityEngine;
 
 namespace mofrison.Network
@@ -8,6 +7,7 @@ namespace mofrison.Network
     {
         public const float MIB = 1048576f;
         public static string cachingDirectory = "data";
+
         public static void ConfiguringCaching(string directoryName)
         {
             cachingDirectory = directoryName;
@@ -17,12 +17,6 @@ namespace mofrison.Network
                 Directory.CreateDirectory(path);
             }
             UnityEngine.Caching.currentCacheForWriting = UnityEngine.Caching.AddCache(path);
-        }
-
-        public static string GetCachedPath(this string url) {
-            string path = url.ConvertToLocalPath();
-            if (File.Exists(path)) return path;
-            else return null;
         }
 
         public static void Caching(string url, byte[] data)
@@ -44,7 +38,34 @@ namespace mofrison.Network
             else { throw new Exception("[Caching] error: Not available space to download " + data.Length / MIB + "Mb"); }
         }
 
-        public static bool CheckFreeSpace(float sizeInBytes) // 1048576f
+        public static string GetCachedPath(this string url)
+        {
+            string path = url.ConvertToLocalPath();
+            if (File.Exists(path)) return path;
+            else return null;
+        }
+
+        public static string ConvertToLocalPath(this string url)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(url))
+                {
+                    var path = Path.Combine(Application.persistentDataPath, cachingDirectory + new System.Uri(url).LocalPath);
+                    return path.Replace("\\", "/");
+                }
+                else
+                {
+                    throw new Exception("[Caching] error: Url address was entered incorrectly " + url); ;
+                }
+            }
+            catch (System.UriFormatException e)
+            {
+                throw new Exception("[Caching] error: " + url + " " + e.Message);
+            }
+        }
+
+        public static bool CheckFreeSpace(float sizeInBytes)
         {
 #if UNITY_EDITOR_WIN
             var logicalDrive = Path.GetPathRoot(Application.persistentDataPath);
@@ -57,38 +78,6 @@ namespace mofrison.Network
         var availableSpace = SimpleDiskUtils.DiskUtils.CheckAvailableSpace(true);
 #endif
             return availableSpace > sizeInBytes / MIB;
-        }
-
-        public static Hash128 GetHashFromCache(string url)
-        {
-            if (string.IsNullOrEmpty(url)) {
-                throw new Exception("[Caching] error: Url address was entered incorrectly " + url);
-            }
-            List<Hash128> listOfCachedVersions = new List<Hash128>();;
-            var uri = new System.Uri(url);
-            UnityEngine.Caching.GetCachedVersions(uri.LocalPath, listOfCachedVersions);
-            if (listOfCachedVersions.Count > 0) return listOfCachedVersions[listOfCachedVersions.Count - 1];
-            else return default;
-        }
-
-        public static string ConvertToLocalPath(this string url)
-        {
-            try
-            {
-                if (!string.IsNullOrEmpty(url))
-                {
-                    var path = Path.Combine(Application.persistentDataPath, cachingDirectory + new System.Uri(url).LocalPath);
-                    return path.Replace("\\","/");
-                }
-                else
-                {
-                    throw new Exception("[Caching] error: Url address was entered incorrectly " + url);;
-                }
-            }
-            catch (System.UriFormatException e)
-            {
-                throw new Exception("[Caching] error: " + url + " " + e.Message);
-            }
         }
 
         public class Exception : System.Exception
