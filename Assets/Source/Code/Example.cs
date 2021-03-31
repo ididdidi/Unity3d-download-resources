c00d9264636cf2b40b5a0c8732fc9226e976f530
mofrison <mofrison@ya.ru>
2021-03-31 14:06:13 +0300

    Rename Network metods

--- Assets/Source/Code/Example.cs
+++ Assets/Source/Code/Example.cs
@@ -39,21 +39,21 @@ namespace mofrison.Network {
 
         private async Task<T> DownloadFromBundle<T>(string url, string name) where T : Object
         {
-            var bundle = await Network.RequestBundle(url, cancelationToken, (prg)=> { print(Path.GetFileName(url) + " " + prg); });
+            var bundle = await Network.GetBundle(url, cancelationToken, (prg)=> { print(Path.GetFileName(url) + " " + prg); });
             loadedBundles.Add(bundle);
             return bundle.LoadAsset<T>(name);
         }
 
         private async Task<Texture> DownloadTexture(string url)
         {
-            var texture = await Network.RequestTexture(url, cancelationToken, (prg) => { print(Path.GetFileName(url) + " " + prg); });
+            var texture = await Network.GetTexture(url, cancelationToken, (prg) => { print(Path.GetFileName(url) + " " + prg); });
             texture.wrapMode = TextureWrapMode.Clamp;
             return texture;
         }
 
         private string DownloadVideo(string url)
         {
-            return Network.RequestVideoStream(url, cancelationToken, (prg) => { print(Path.GetFileName(url) + " " + prg); });
+            return Network.GetVideoStream(url, cancelationToken, (prg) => { print(Path.GetFileName(url) + " " + prg); });
         }
 
         private void OnDestroy()
--- Assets/Source/Code/Network.cs
+++ Assets/Source/Code/Network.cs
@@ -8,16 +8,99 @@ namespace mofrison.Network
 {
     public static class Network
     {
-        public static async Task<AssetBundle> RequestBundle(string url, CancellationTokenSource cancelationToken, System.Action<float> progress = null, bool caching = true)
+        public static async Task<byte[]> GetData(string url, CancellationTokenSource cancelationToken, System.Action<float> progress = null)
+        {
+            UnityWebRequest uwr = await SendWebRequest(UnityWebRequest.Get(url), cancelationToken, progress);
+            if (uwr != null && !uwr.isHttpError && !uwr.isNetworkError)
+            {
+                return uwr.downloadHandler.data;
+            }
+            else
+            {
+                throw new Exception("[Netowrk] error: " + uwr.error + " " + uwr.uri);
+            }
+        }
+
+        public static async Task<Texture2D> GetTexture(string url, CancellationTokenSource cancelationToken, System.Action<float> progress = null, bool caching = true)
+        {
+            UnityWebRequest request;
+
+            string path = url.GetCachedPath();
+            if (string.IsNullOrEmpty(path)) { request = UnityWebRequestTexture.GetTexture(url); }
+            else { request = UnityWebRequestTexture.GetTexture("file://" + path); progress = null; }
+
+            UnityWebRequest uwr = await SendWebRequest(request, cancelationToken, progress);
+            if (uwr != null && !uwr.isHttpError && !uwr.isNetworkError)
+            {
+                Texture2D texture = DownloadHandlerTexture.GetContent(uwr);
+                texture.name = Path.GetFileName(uwr.url);
+                if (caching && ResourceCache.CheckFreeSpace(await GetSize(url))) 
+                {
+                    ResourceCache.Caching(uwr.url, uwr.downloadHandler.data);
+                }
+                return texture;
+            }
+            else
+            {
+                throw new Exception("[Netowrk] error: " + uwr.error + " " + uwr.uri);
+            }
+        }
+
+        public static async Task<AudioClip> GetAudioClip(string url, CancellationTokenSource cancelationToken, System.Action<float> progress = null, bool caching = true, AudioType audioType = AudioType.OGGVORBIS)
+        {
+            UnityWebRequest request;
+            string path = url.GetCachedPath();
+            if (string.IsNullOrEmpty(path)) { request = UnityWebRequestMultimedia.GetAudioClip(url, audioType); }
+            else { request = UnityWebRequestMultimedia.GetAudioClip("file://" + path, audioType); progress = null; }
+
+            UnityWebRequest uwr = await SendWebRequest(request, cancelationToken, progress);
+            if (uwr != null && !uwr.isHttpError && !uwr.isNetworkError)
+            {
+                AudioClip audioClip = DownloadHandlerAudioClip.GetContent(uwr);
+                audioClip.name = Path.GetFileName(uwr.url);
+
+                if (caching && ResourceCache.CheckFreeSpace(await GetSize(url))) 
+                { 
+                    ResourceCache.Caching(uwr.url, uwr.downloadHandler.data); 
+                }
+                return audioClip;
+            }
+            else
+            {
+                throw new Exception("[Netowrk] error: " + uwr.error + " " + uwr.uri);
+            }
+        }
+
+        private delegate void AsyncOperation();
+
+        public static string GetVideoStream(string url, CancellationTokenSource cancelationToken, System.Action<float> progress = null, bool caching = true)
+        {
+            string path = url.GetCachedPath();
+            if (string.IsNullOrEmpty(path))
+            {
+                AsyncOperation cachingVideo = async delegate {
+                    if (caching && ResourceCache.CheckFreeSpace(await GetSize(url)))
+                    {
+                    
+                        ResourceCache.Caching(url, await GetData(url, cancelationToken, progress));
+                    }
+                };
+                cachingVideo();
+                return url;
+            }
+            else { return path; }
+        }
+
+        public static async Task<AssetBundle> GetBundle(string url, CancellationTokenSource cancelationToken, System.Action<float> progress = null, bool caching = true)
         {
             UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(url); ;
             CachedAssetBundle cachedAssetBundle = await GetCachedAssetBundle(new System.Uri(url));
-            if (Caching.IsVersionCached(cachedAssetBundle) || (caching && ResourceCache.CheckFreeSpace(await RequestSize(url))))
+            if (Caching.IsVersionCached(cachedAssetBundle) || (caching && ResourceCache.CheckFreeSpace(await GetSize(url))))
             {
                 request = UnityWebRequestAssetBundle.GetAssetBundle(url, cachedAssetBundle, 0);
             }
 
-            UnityWebRequest uwr = await WebRequest(request, cancelationToken, Caching.IsVersionCached(cachedAssetBundle)? null : progress);
+            UnityWebRequest uwr = await SendWebRequest(request, cancelationToken, Caching.IsVersionCached(cachedAssetBundle)? null : progress);
             if (uwr != null && !uwr.isHttpError && !uwr.isNetworkError)
             {
                 AssetBundle assetBundle = DownloadHandlerAssetBundle.GetContent(uwr);
@@ -34,84 +117,7 @@ namespace mofrison.Network
             }
         }
 
-        public static async Task<Texture2D> RequestTexture(string url, CancellationTokenSource cancelationToken, System.Action<float> progress = null, bool caching = true)
-        {
-            UnityWebRequest request;
-
-            string path = url.GetCachedPath();
-            if (string.IsNullOrEmpty(path)) { request = UnityWebRequestTexture.GetTexture(url); }
-            else { request = UnityWebRequestTexture.GetTexture("file://" + path); progress = null; }
-
-            UnityWebRequest uwr = await WebRequest(request, cancelationToken, progress);
-            if (uwr != null && !uwr.isHttpError && !uwr.isNetworkError)
-            {
-                Texture2D texture = DownloadHandlerTexture.GetContent(uwr);
-                texture.name = Path.GetFileName(uwr.url);
-                if (caching) { ResourceCache.Caching(uwr.url, uwr.downloadHandler.data); }
-                return texture;
-            }
-            else
-            {
-                throw new Exception("[Netowrk] error: " + uwr.error + " " + uwr.uri);
-            }
-        }
-
-        public static async Task<AudioClip> RequestAudioClip(string url, CancellationTokenSource cancelationToken, System.Action<float> progress = null, bool caching = true, AudioType audioType = AudioType.OGGVORBIS)
-        {
-            UnityWebRequest request;
-            string path = url.GetCachedPath();
-            if (string.IsNullOrEmpty(path)) { request = UnityWebRequestMultimedia.GetAudioClip(url, audioType); }
-            else { request = UnityWebRequestMultimedia.GetAudioClip("file://" + path, audioType); progress = null; }
-
-            UnityWebRequest uwr = await WebRequest(request, cancelationToken, progress);
-            if (uwr != null && !uwr.isHttpError && !uwr.isNetworkError)
-            {
-                AudioClip audioClip = DownloadHandlerAudioClip.GetContent(uwr);
-                audioClip.name = Path.GetFileName(uwr.url);
-
-                if (caching) { ResourceCache.Caching(uwr.url, uwr.downloadHandler.data); }
-                return audioClip;
-            }
-            else
-            {
-                throw new Exception("[Netowrk] error: " + uwr.error + " " + uwr.uri);
-            }
-        }
-
-
-        private delegate void AsyncOperation();
-
-        public static string RequestVideoStream(string url, CancellationTokenSource cancelationToken, System.Action<float> progress = null, bool caching = true)
-        {
-            string path = url.GetCachedPath();
-            if (string.IsNullOrEmpty(path))
-            {
-                if (caching)
-                {
-                    AsyncOperation cachingVideo = async delegate {
-                        ResourceCache.Caching(url, await RequestData(url, cancelationToken, progress));
-                    };
-                    cachingVideo();
-                }
-                return url;
-            }
-            else { return path; }
-        }
-
-        public static async Task<byte[]> RequestData(string url, CancellationTokenSource cancelationToken, System.Action<float> progress = null)
-        {
-            UnityWebRequest uwr = await WebRequest(UnityWebRequest.Get(url), cancelationToken, progress);
-            if (uwr != null && !uwr.isHttpError && !uwr.isNetworkError)
-            {
-                return uwr.downloadHandler.data;
-            }
-            else
-            {
-                throw new Exception("[Netowrk] error: " + uwr.error + " " + uwr.uri);
-            }
-        }
-
-        private static async Task<UnityWebRequest> WebRequest(UnityWebRequest request, CancellationTokenSource cancelationToken = null, System.Action<float> progress = null)
+        private static async Task<UnityWebRequest> SendWebRequest(UnityWebRequest request, CancellationTokenSource cancelationToken = null, System.Action<float> progress = null)
         {
             while (!Caching.ready)
             {
@@ -146,9 +152,9 @@ namespace mofrison.Network
             return request;
         }
 
-        private static async Task<int> RequestSize(string url)
+        private static async Task<int> GetSize(string url)
         {
-            UnityWebRequest request = await WebRequest(UnityWebRequest.Head(url));
+            UnityWebRequest request = await SendWebRequest(UnityWebRequest.Head(url));
             var contentLength = request.GetResponseHeader("Content-Length");
             if (int.TryParse(contentLength, out int returnValue))
             {
@@ -160,9 +166,9 @@ namespace mofrison.Network
             }
         }
 
-        private static async Task<string> RequestText(string url)
+        private static async Task<string> GetText(string url)
         {
-            var uwr = await WebRequest(UnityWebRequest.Get(url));
+            var uwr = await SendWebRequest(UnityWebRequest.Get(url));
             if (uwr != null && !uwr.isHttpError && !uwr.isNetworkError)
             {
                 return uwr.downloadHandler.text;
@@ -185,7 +191,7 @@ namespace mofrison.Network
         private static async Task<CachedAssetBundle> GetCachedAssetBundle(System.Uri uri)
         {
             Hash128 hash = default;
-            string manifest = await RequestText(uri + ".manifest");
+            string manifest = await GetText(uri + ".manifest");
 
             if (!string.IsNullOrEmpty(manifest))
             {
